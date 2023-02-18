856fb2fafda6cebca03e1ac449413de1f12916c7
Faffy0001 <qfaffy@gmail.com>
2023-02-18 14:59:27 -0800

    reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char h;
+
+	h = 'z';
+	while
+		(h >= 'a') {
+			putchar(h);
+			h++;
+		}
+	putchar('\n');
+	return (0);
+}
