fbb53e9b47ef9e7bffa96852220f90144b2376a7
Faffy0001 <qfaffy@gmail.com>
2023-02-23 16:32:53 -0800

    Loops and functions

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,31 +3,30 @@
 #define _MAIN_H_
 
 
-char *_strcat(char *dest, char *src);
 
-char *_strncat(char *dest, char *src, int n);
+int _isupper(int c);
 
-char *_strncpy(char *dest, char *src, int n);
+int _isdigit(int c);
 
-int _strcmp(char *s1, char *s2);
+int mul(int a, int b);
 
-void reverse_array(int *a, int n);
-
-char *string_toupper(char *);
-
-char *cap_string(char *);
-
-char *leet(char *);
-
-char *rot13(char *);
-
-void print_number(int n);
+void print_numbers(void);
 
 int _putchar(char);
 
-char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_most_numbers(void);
 
-void print_buffer(char *b, int size);
+void more_numbers(void);
+
+void print_line(int n);
+
+void print_diagonal(int n);
+
+void print_square(int size);
+
+void print_triangle(int size);
+
+void print_number(int n);
 
 
 
