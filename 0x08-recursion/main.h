4e75f31c5bbb4905ac91d3e4fc9ca1e379709786
Faffy0001 <qfaffy@gmail.com>
2023-03-09 15:47:33 -0800

    0-puts_recursion.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,13 +1,13 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
-void_puts_recursion(char *s);
-int_putchar(char c);
-void_print_rev_recursion(char *s);
-int_strlen_recursion(char *s);
+int _putchar(char);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
 int factorial(int n);
-int_pow_recursion(int x, int y);
-int_sqrt_recursion(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
 int is_prime_number(int n);
-int is_palindrome(char *s);
+
 #endif
