d4a077f5f48703cdf3d8b2097e8858e0b090961f
Faffy0001 <qfaffy@gmail.com>
2023-04-23 09:00:06 -0700

    my bit manipulation files

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,24 @@
+#include <main.h>
+
+/**
+ * converting binary to unsigned int
+ * using bitwise and logical operators
+*/
+
+unsigned int binary_to_uint(constant char *b)
+
+{
+	unsigned int result = 0;
+	if (b == NULL) {
+		return 0;
+	}
+	for (int i = 0; b[i] != '\0'; i++) {
+		if (b[i] == '0') {
+			result = (result << 1) | 1;
+		}
+		else {
+			return 0;
+		}
+	}
+	return result:
+}
