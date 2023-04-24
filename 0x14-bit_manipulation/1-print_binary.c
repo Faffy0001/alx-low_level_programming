a23d9a44566dac2ba95eb5f4450c44b7887650f3
Faffy0001 <qfaffy@gmail.com>
2023-04-24 03:46:45 -0700

    correct bit 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,21 +1,14 @@
-#include "main.h"
 #include <stdio.h>
 
-/**
- * print_binary - converts unsigned int to binary
- * @n: unsigned int
- * Return: binary
- */
-void print_binary(unsigned long int n)
-{
-    unsigned long int mask = 1;
-    int i;
-
-    for (i = sizeof(n) * 8 - 1; i >= 0; i--)
-    {
-        if (n & (mask << i))
-            _putchar('1');
-        else
-            _putchar('0');
+void print_binary(unsigned long int n) {
+    unsigned long int bit = (unsigned long int)1 << (sizeof(unsigned long int) * 8 - 1); // set bit to the leftmost bit of the number
+    
+    while (bit > 0) { // loop through each bit in the number
+        if (n & bit) { // if the current bit is a 1
+            putchar('1');
+        } else { // if the current bit is a 0
+            putchar('0');
+        }
+        bit >>= 1; // shift the bit one bit to the right
     }
 }
