0dfaafab95b938e675e2d1957701737522e0b4e9
Faffy0001 <qfaffy@gmail.com>
2023-04-24 18:22:23 -0700

    get bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -2,18 +2,20 @@
 
 /**
  * Return the value of the bit at the given index
- * Return: the value of the bit at index index or -1 if an error occured
+ * Return: the value of the bit at index or -1 if an error occured
  */
 
 int get_bit(unsigned long int n, unsigned int index)
 
 {
-	if (index >= 63)
-	{
-		return (n >> index) & 1;
-	}
-	else
+	if (index >= sixeof(unsigned long) * 8)
 	{
 		return -1;
 	}
+	
+	int bin = n >> /*right shift bitwise operator*/
+	int bit = bin & 1;
+
+	return bit;
 }
+			 
