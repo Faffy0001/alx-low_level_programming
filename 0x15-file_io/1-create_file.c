2f063fb50d669320cbaa2262f53d44f0207ab7e9
Faffy0001 <qfaffy@gmail.com>
2023-04-26 14:51:32 -0700

    Hot Fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -5,16 +5,16 @@
  * @filename: is the name of the file to create 
  * Return: 1 on success, -1 on failure
  */
-
 int create_file(const char *filename, char *text_content)
 {
-	int fd, ret_write;
-	size_t strlength = 0;
+	char *buffer;
+	int fd, i, strlength = 0;
+	int ret_write;
 
-	id (filename == NULL)
+	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, 0_WRONLY | 0_CREAT | 0_TRUNC, 0600);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 
@@ -24,12 +24,18 @@ int create_file(const char *filename, char *text_content)
 		return (1);
 	}
 
-	while (text_content[strlenght])
+	for (i = 0; text_content[i] != '\0'; i++)
 		strlength++;
 
-	ret_write = write(fd, text_content, strlength);
+	buffer = malloc(sizeof(char) * strlength + 1);
+	if (buffer == NULL)
+		return (-1);
+	buffer = text_content;
+
+	ret_write = write(fd, buffer, strlength);
 	if (ret_write == -1)
 	{
+		free(buffer);
 		close(fd);
 		return (-1);
 	}
