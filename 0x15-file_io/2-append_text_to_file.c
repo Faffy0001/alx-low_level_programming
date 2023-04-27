ec0ed733cbd5007430cffd123fbdebb684c6f666
Faffy0001 <qfaffy@gmail.com>
2023-04-26 18:06:33 -0700

    append text file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -8,37 +8,53 @@
  */
 
 int append_text_to_file(const char *filename, char *text_content)
-
-int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
-	int j, strlength = 0;
-	int write_n;
+	ssize_t w;
+	int size;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
 
 	fd = open(filename, O_WRONLY | O_APPEND);
+
 	if (fd == -1)
 		return (-1);
 
-	if (text_content == NULL)
+	if (!text_content)
 	{
 		close(fd);
 		return (1);
 	}
 
-	for (j = 0; text_content[j] != '\0'; j++)
-		strlength++;
+	size = _strlen(text_content);
+	w = write(fd, text_content, size);
 
-	write_n = write(fd, text_content, strlength);
-	if (write_n == -1 || write_n != strlength)
+	if (w == -1)
 	{
 		close(fd);
 		return (-1);
 	}
-
-
 	close(fd);
 	return (1);
-}	
+}
+
+/**
+ * _strlen - len
+ *
+ * @s: is a pointer to a char
+ *
+ * Return: Always 0.
+ */
+
+int _strlen(const char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
